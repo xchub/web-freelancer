fdebf75c826fc5686ab1b1734894c229147b33f7
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2015-12-28 20:56:43 -0500

    Menu added for clients management

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,20 @@
 @section('content')
 <div class="container spark-screen">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-2 sidebar">
+            <div class="panel panel-default">
+                <div class="panel-heading">Menu</div>
+
+                <div class="panel-body">
+                    <ul class="nav nav-sidebar">
+                        <li>Clients</li>
+                        <li><a href="#">List</a></li>
+                        <li><a href="#">Add new</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-10">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
