185df30257a3c37c340d8edcc1b1080aca498d75
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2015-12-28 20:20:36 -0500

    basic view tweak: adding includes

---
+++ resources/views/includes/nav/header-left.blade.php
@@ -0,0 +1,14 @@
+<div class="navbar-header">
+    <!-- Collapsed Hamburger -->
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+            <span class="sr-only">Toggle Navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+
+    <!-- Branding Image -->
+    <a class="navbar-brand" href="{{ url('/') }}">
+        {!! Config::appName() !!}
+    </a>
+</div>
