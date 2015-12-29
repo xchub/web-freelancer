01b1e1e0684f2a660d8860703bee254916cce8e8
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2015-12-29 14:22:16 -0500

    invoices table

---
+++ database/migrations/2015_12_29_185705_create_invoices_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('reference_nb');
+            $table->integer('client_id');
+            $table->date('due_date');
+            $table->text('notes')->nullable();
+            $table->float('tax');
+            $table->boolean('recurring'); // yes or no
+            $table->integer('recurring_frequency')->nullable(); // how many days between invoicing?
+            $table->date('recurring_start')->nullable();
+            $table->date('recurring_end')->nullable();
+            $table->date('recurring_next')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('invoices');
+    }
+}
---
+++ database/migrations/2015_12_29_191425_create_invoice_item_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoiceItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('invoice_id');
+            $table->string('name');
+            $table->string('description');
+            $table->float('cost');
+            $table->float('quantity');
+            $table->float('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('invoice_items');
+    }
+}
