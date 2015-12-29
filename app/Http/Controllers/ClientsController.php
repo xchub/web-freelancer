9cd8a0d26d1bb11b54b7071f42fb13ea2ed3596f
jpmurray <jeanphilippe@pasunecompagnie.com>
2015-12-29 12:08:57 -0500

    Applied fixes from StyleCI

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-
 use App\Client;
 use Session;
 use Redirect;
@@ -24,120 +22,123 @@ class ClientsController extends Controller
 
     /**
      * Show the application dashboard.
-     * GET: /client
+     * GET: /client.
      *
      * @return Response
      */
     public function index()
     {
         $clients = Client::paginate(2);
+
         return view('clients.index')->with('clients', $clients);
     }
 
-    /**
+  /**
    * Show the form for creating a new resource.
-   * GET: /client/create
+   * GET: /client/create.
    *
    * @return Response
    */
   public function create()
   {
-   foreach (Countries::getList('name') as $key => $value) {
-     $countries[$key] = $value['name'];
-   }
+      foreach (Countries::getList('name') as $key => $value) {
+          $countries[$key] = $value['name'];
+      }
 
-    return view('clients.create')->with('countries', $countries);
+      return view('clients.create')->with('countries', $countries);
   }
 
   /**
    * Store a newly created resource in storage.
-   * POST: /client
+   * POST: /client.
    *
    * @return Response
    */
   public function store(Request $request)
   {
-    $this->validate($request, [
+      $this->validate($request, [
         'name' => 'required',
         'email' => 'required|email',
         'website' => 'url',
     ]);
 
-    $input = $request->all();
-    $client = Client::create($input);
-    $client->save();
+      $input = $request->all();
+      $client = Client::create($input);
+      $client->save();
 
-    Session::flash('success', 'Client successfully added.');
-    return Redirect::route('client.index');
+      Session::flash('success', 'Client successfully added.');
+
+      return Redirect::route('client.index');
   }
 
   /**
    * Display the specified resource.
-   * GET: /client/{client}
+   * GET: /client/{client}.
    *
    * @param  int  $id
    * @return Response
    */
   public function show($id)
   {
-    $client = Client::findOrFail($id);
-    return view('clients.show')->with('client', $client);
+      $client = Client::findOrFail($id);
+
+      return view('clients.show')->with('client', $client);
   }
 
   /**
    * Show the form for editing the specified resource.
-   * GET: /client/{client}/edit
+   * GET: /client/{client}/edit.
    *
    * @param  int  $id
    * @return Response
    */
   public function edit($id)
   {
-    foreach (Countries::getList('name') as $key => $value) {
-     $countries[$key] = $value['name'];
-   }
+      foreach (Countries::getList('name') as $key => $value) {
+          $countries[$key] = $value['name'];
+      }
 
-    $client = Client::findOrFail($id);
-    return view('clients.edit')->with('id', $id)->with('client', $client)->with('countries', $countries);
+      $client = Client::findOrFail($id);
+
+      return view('clients.edit')->with('id', $id)->with('client', $client)->with('countries', $countries);
   }
 
   /**
    * Update the specified resource in storage.
-   * PUIT/PATCH: /client/{client}
+   * PUIT/PATCH: /client/{client}.
    *
    * @param  int  $id
    * @return Response
    */
   public function update(Request $request, $id)
   {
-    $this->validate($request, [
+      $this->validate($request, [
         'name' => 'required',
         'email' => 'required|email',
         'website' => 'url',
     ]);
 
-    $input = $request->all();
-    $client = Client::findOrFail($id);
-    $client->fill($input)->save();
+      $input = $request->all();
+      $client = Client::findOrFail($id);
+      $client->fill($input)->save();
 
-    Session::flash('success', 'Client modification saved!');
-    return Redirect::route('client.show', ['id' => $id]);
-    
+      Session::flash('success', 'Client modification saved!');
+
+      return Redirect::route('client.show', ['id' => $id]);
   }
 
   /**
    * Remove the specified resource from storage.
-   * DELETE: /client/{client}
+   * DELETE: /client/{client}.
    *
    * @param  int  $id
    * @return Response
    */
   public function destroy($id)
   {
+      Client::destroy($id);
+      Session::flash('success', 'Client successfully destroyed.');
 
-    Client::destroy($id);
-    Session::flash('success', 'Client successfully destroyed.');
-    return Redirect::route('client.index');
-    
+      return Redirect::route('client.index');
   }
 }
--- database/migrations/2015_12_29_013627_add_clients_table.php
+++ database/migrations/2015_12_29_013627_add_clients_table.php
@@ -16,13 +16,13 @@ class AddClientsTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->nullable();
             $table->string('name');
-            $table->string('contact')->nullable();;
-            $table->string('email')->nullable();;
-            $table->string('website')->nullable();;
-            $table->string('phone', 64)->nullable();;
-            $table->string('address')->nullable();;
-            $table->string('city')->nullable();;
-            $table->string('country')->nullable();;
+            $table->string('contact')->nullable();
+            $table->string('email')->nullable();
+            $table->string('website')->nullable();
+            $table->string('phone', 64)->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
             $table->timestamps();
         });
     }
