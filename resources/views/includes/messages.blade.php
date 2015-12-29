4d0e9018d8ab1ffe39f3ec9841827a294b1bd095
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2015-12-28 22:16:06 -0500

    modified layout to include messages and page specific scripts

---
+++ resources/views/includes/messages.blade.php
@@ -0,0 +1,31 @@
+    @if (session('success'))
+    <div class="alert alert-success alert-dismissable" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h3 class="font-w300 push-15">Win!</h3>
+        <p>{{ session('success') }}</p>
+    </div>
+    @endif
+
+    @if (session('info'))
+    <div class="alert alert-info alert-dismissable" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h3 class="font-w300 push-15">FYI</h3>
+        <p>{{ session('info') }}</p>
+    </div>
+    @endif
+
+    @if (session('warning'))
+    <div class="alert alert-warning alert-dismissable" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h3 class="font-w300 push-15">Look out!</h3>
+        <p>{{ session('warning') }}</p>
+    </div>
+    @endif
+
+    @if (session('fail'))
+    <div class="alert alert-danger alert-dismissable" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h3 class="font-w300 push-15">Fumble :(</h3>
+        <p>{{ session('fail') }}</p>
+    </div>
+    @endif
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,7 @@
     </nav>
     <div class="container spark-screen">
         <div class="row">
+            @include('includes.messages')
             @include('includes.nav.sidebar')
         <div class="col-md-10">
             <div class="panel panel-default">
@@ -50,5 +51,6 @@
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    @yield('page_scripts')
 </body>
 </html>
